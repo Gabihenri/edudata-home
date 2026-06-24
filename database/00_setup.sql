7300f637764e02a707d0779981a384e66665249d
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-06-24 11:01:20 -0300

    Atualizar o 00_setup.sql

--- database/00_setup.sql
+++ database/00_setup.sql
@@ -1,1 +1,16 @@
-.
+-- =====================================================
+-- EDUData IA
+-- EDU-SQL-001
+-- Setup Inicial
+-- =====================================================
+
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
+-- Atualiza automaticamente o campo updated_at
+CREATE OR REPLACE FUNCTION set_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
