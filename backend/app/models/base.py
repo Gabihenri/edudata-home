b385c3f40b7819f06e4d738555c0f9d68aa40cea
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-06-24 15:24:25 -0300

    Atualizar o base.py

--- backend/app/models/base.py
+++ backend/app/models/base.py
@@ -1,15 +1,4 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
 
-DATABASE_URL = "postgresql://usuario:senha@localhost/edudata"
-
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
+class Base(DeclarativeBase):
+    pass
