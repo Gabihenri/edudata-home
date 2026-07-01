00f15d15e044977fd178377c1384ba6b9e844191
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-07-01 10:30:08 -0300

    Update page.tsx

--- src/app/academy/page.tsx
+++ src/app/academy/page.tsx
@@ -1,19 +1,1 @@
-import Header from '@/components/layout/Header'
-import Footer from '@/components/layout/Footer'
-import AccessibilityBar from '@/components/layout/AccessibilityBar'
-import AcademySection from '@/components/academy/AcademySection'
 
-export default function AcademyPage() {
-  return (
-    <>
-      <AccessibilityBar />
-      <Header />
-
-      <main>
-        <AcademySection />
-      </main>
-
-      <Footer />
-    </>
-  )
-}
