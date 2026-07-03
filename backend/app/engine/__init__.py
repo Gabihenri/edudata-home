338aa5dc63ee993e2c9081e79c16993ca266efe5
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-07-03 14:21:46 -0300

    Atualizar o __init__.py

--- backend/app/engine/__init__.py
+++ backend/app/engine/__init__.py
@@ -1,1 +1,9 @@
+from app.engine.context import EngineContext
+from app.engine.engine import EDIEngine
+from app.engine.registry import EngineRegistry
 
+__all__ = [
+    "EDIEngine",
+    "EngineContext",
+    "EngineRegistry",
+]
