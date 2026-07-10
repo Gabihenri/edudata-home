f4fe9751dd6c1bfcc59ba93c60b18d73412a39c3
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-07-10 09:00:31 -0300

    Criar index.ts

---
+++ src/lib/agenda/index.ts
@@ -0,0 +1,8 @@
+export { eventsRepository } from './repository/events.repository'
+export { eventsService } from './services/events.service'
+
+export type {
+  AgendaEvent,
+  CreateAgendaEventInput,
+  UpdateAgendaEventInput,
+} from './repository/events.repository'
