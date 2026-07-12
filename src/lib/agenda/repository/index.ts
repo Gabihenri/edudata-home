44775d8196252448be41add48e8947afbbb413f5
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-07-12 11:15:05 -0300

    Atualizar o index.ts

--- src/lib/agenda/repository/index.ts
+++ src/lib/agenda/repository/index.ts
@@ -1,7 +1,35 @@
 export { eventsRepository } from './events.repository'
+export { tasksRepository } from './tasks.repository'
+export { planningRepository } from './planning.repository'
+export { evidencesRepository } from './evidences.repository'
+export { classesRepository } from './classes.repository'
 
 export type {
   AgendaEvent,
   CreateAgendaEventInput,
   UpdateAgendaEventInput,
 } from './events.repository'
+
+export type {
+  AgendaTask,
+  CreateAgendaTaskInput,
+  UpdateAgendaTaskInput,
+} from './tasks.repository'
+
+export type {
+  AgendaPlanning,
+  CreateAgendaPlanningInput,
+  UpdateAgendaPlanningInput,
+} from './planning.repository'
+
+export type {
+  AgendaEvidence,
+  CreateAgendaEvidenceInput,
+  UpdateAgendaEvidenceInput,
+} from './evidences.repository'
+
+export type {
+  AgendaClass,
+  CreateAgendaClassInput,
+  UpdateAgendaClassInput,
+} from './classes.repository'
