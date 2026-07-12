93f7b4dfa58f55539bfd0b7e7f602790484c7e32
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-07-12 19:18:29 -0300

    Atualizar o index.ts

--- src/lib/agenda/services/index.ts
+++ src/lib/agenda/services/index.ts
@@ -3,3 +3,8 @@ export { tasksService } from './tasks.service'
 export { planningService } from './planning.service'
 export { evidencesService } from './evidences.service'
 export { classesService } from './classes.service'
+export { dashboardService } from './dashboard.service'
+
+export type {
+  AgendaDashboardSummary,
+} from './dashboard.service'
