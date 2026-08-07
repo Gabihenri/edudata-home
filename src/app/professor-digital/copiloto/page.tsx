ef3483443f0eb4c978e65ef4ab0ac0e02462dc8a
Gabihenri <51890771+Gabihenri@users.noreply.github.com>
2026-08-07 10:11:13 -0300

    Criar rota do Copiloto Pedagógico

---
+++ src/app/professor-digital/copiloto/page.tsx
@@ -0,0 +1,11 @@
+import PedagogicalCopilotHub from '@/components/professor-digital/PedagogicalCopilotHub'
+
+export const metadata = {
+  title: 'Copiloto Pedagógico | Professor Digital',
+  description:
+    'Centro do Copiloto Pedagógico do Professor Digital, integrado à Agenda Inteligente EDI e ao EIOS.',
+}
+
+export default function PedagogicalCopilotPage() {
+  return <PedagogicalCopilotHub />
+}
